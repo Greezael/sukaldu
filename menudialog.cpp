58c80bc5b01367623a3e4e05e075400494d84c40
ddominguez <dominguezcatenadaniel@gmail.com>
2015-04-05 16:52:53 +0200

    Menudialog.cpp

---
+++ menudialog.cpp
@@ -0,0 +1,144 @@
+// TORREMOVE
+#include <iostream>
+#include <sstream>
+
+#include "menudialog.h"
+#include "ui_menudialog.h"
+
+#include "mainwindow.h"
+
+#include <QtSql>
+
+MenuDialog::MenuDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::MenuDialog)
+{
+    ui->setupUi(this);
+}
+
+MenuDialog::MenuDialog(int menuIdentifier, int roleIdentifier, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::MenuDialog)
+{
+    this->menuId = menuIdentifier;
+    this->roleId = roleIdentifier;
+    ui->setupUi(this);
+
+    fillCategoryList();
+    QObject::connect(this->ui->menurec_cat,
+                     SIGNAL(currentIndexChanged(int)),
+                     this,
+                     SLOT(catSelected(int)));
+    QObject::connect(this->ui->menurec_subcat,
+                     SIGNAL(currentIndexChanged(int)),
+                     this,
+                     SLOT(subcatSelected(int)));
+    QObject::connect(this, SIGNAL(accepted()), this, SLOT(addRecipe()));
+}
+
+
+void MenuDialog::fillCategoryList()
+{
+    QComboBox *cat, *subcat;
+    cat = this->ui->menurec_cat;
+    subcat = this->ui->menurec_subcat;
+
+    cat->blockSignals(true);
+    cat->clear();
+
+    QSqlQuery query;
+    query.prepare("SELECT id, name FROM recipe_cat");
+    query.exec();
+    cat->addItem("Any", QVariant::fromValue(-1));
+    while (query.next())
+    {
+        cat->addItem(query.value("name").toString(), QVariant::fromValue(query.value("id").toInt()));
+    }
+    cat->setCurrentIndex(0);
+    cat->blockSignals(false);
+    catSelected(0);
+}
+
+void MenuDialog::catSelected(int index)
+{
+    QComboBox *cat, *subcat;
+    cat = this->ui->menurec_cat;
+    subcat = this->ui->menurec_subcat;
+
+    subcat->blockSignals(true);
+    subcat->clear();
+
+    int catId = cat->itemData(index).toInt();
+
+    if (catId != -1)
+    {
+        QSqlQuery query;
+        query.prepare("SELECT id, name FROM recipe_subcat WHERE cat = :catId");
+        query.bindValue(":catId", QVariant::fromValue(catId));
+        query.exec();
+        subcat->addItem("Any", QVariant::fromValue(-1));
+        while (query.next())
+        {
+            subcat->addItem(query.value("name").toString(), QVariant::fromValue(query.value("id").toInt()));
+        }
+        subcat->setCurrentIndex(0);
+    }
+    else
+    {
+        subcat->addItem("Any", QVariant::fromValue(-1));
+    }
+    subcat->blockSignals(false);
+    subcatSelected(0);
+}
+
+void MenuDialog::subcatSelected(int index)
+{
+    QComboBox *cat, *subcat, *rec;
+    cat = this->ui->menurec_cat;
+    subcat = this->ui->menurec_subcat;
+    rec = this->ui->menurec_rec;
+
+    rec->clear();
+
+    QVariant catId = cat->currentData();
+    QVariant subcatId = subcat->currentData();
+
+    QString catCond = (catId.toInt() >= 0) ? "WHERE cat = :catId " : "";
+    QString subcatCond = ((catId.toInt() >= 0) && (subcatId.toInt() >= 0)) ?
+                "AND subcat = :subcatId " : "";
+
+    QSqlQuery query;
+    query.prepare("SELECT id, name FROM recipe " +
+                  catCond +
+                  subcatCond);
+    query.bindValue(":catId", catId);
+    query.bindValue(":subcatId", subcatId);
+    query.exec();
+    while (query.next())
+    {
+        rec->addItem(query.value("name").toString(), QVariant::fromValue(query.value("id").toInt()));
+    }
+    rec->setCurrentIndex(0);
+}
+
+void MenuDialog::addRecipe()
+{
+    if (!this->ui->menurec_rec->currentData().isNull())
+    {
+        int recId = this->ui->menurec_rec->currentData().toInt();
+        QSqlQuery query;
+        query.prepare("INSERT INTO menu_recipe VALUES (:menu, :recipe, :role)");
+        query.bindValue(":menu", menuId);
+        query.bindValue(":recipe", recId);
+        query.bindValue(":role", roleId);
+        query.exec();
+    }
+
+    MainWindow * mainwindow = (MainWindow *) this->parent();
+    mainwindow->reloadMenuOptions();
+}
+
+MenuDialog::~MenuDialog()
+{
+    delete ui;
+}
